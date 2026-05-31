a034cb8fea52b3f42aaf8751ee4550708b6cafa4
diffth <diffth@nate.com>
2026-05-31 13:10:17 +0900

    15

---
+++ langchain-openai-claude.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableBranch, RunnablePassthrough
+
